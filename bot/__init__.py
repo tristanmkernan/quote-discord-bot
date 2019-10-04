8b3b0164c9104d76a306632d8cd35cd7a64e911f
Tristan K <tristanmkernan@gmail.com>
2019-10-04 18:29:07 -0400

    feat: error handling, help text

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,6 +10,7 @@ from .datastore import (
     save_quote_log,
     delete_quote,
 )
+from .datastore.database import Quote as QuoteModel, QuoteMessageLog
 
 bot = commands.Bot("!")
 
@@ -17,7 +18,10 @@ bot = commands.Bot("!")
 @bot.command()
 async def squote(ctx: commands.Context, author: discord.Member, *quote: str):
     """
-    Saves the given quote
+    Saves the quote by the @author
+
+    :param author: tag or @ the author
+    :param quote: quotes are optional
     """
     quote_combined: str = " ".join(quote)
     invoker: discord.Member = ctx.author
@@ -30,6 +34,9 @@ async def squote(ctx: commands.Context, author: discord.Member, *quote: str):
 
 @squote.error
 async def squote_error(ctx: commands.Context, error):
+    """
+    Handles parameter errors for `squote`, for example when `author` is an invalid Member
+    """
     if isinstance(error, commands.BadArgument):
         await ctx.send("I could not find that member...")
 
@@ -38,15 +45,21 @@ async def squote_error(ctx: commands.Context, error):
 async def rquote(ctx: commands.Context, author: discord.Member = None):
     """
     Retrieves a random quote [by author, if supplied]
+
+    :param author: [optional] - tag or @ the author
     """
     guild: discord.Guild = ctx.guild
-    quote: Quote = await get_random_quote(author, guild)
 
-    message = f"{quote.content} - _{quote.author}_, {quote.timestamp.year}"
+    try:
+        quote: Quote = await get_random_quote(author, guild)
+    except QuoteModel.DoesNotExist:
+        await ctx.send("No quotes in the database!")
+    else:
+        message = f"{quote.content} - _{quote.author}_, {quote.timestamp.year}"
 
-    res: discord.Message = await ctx.send(message)
+        res: discord.Message = await ctx.send(message)
 
-    await save_quote_log(res, quote)
+        await save_quote_log(res, quote)
 
 
 @bot.command()
@@ -61,26 +74,23 @@ async def quotestats(ctx: commands.Context):
     await ctx.send(message)
 
 
-@bot.command()
-async def quotehelp(ctx: commands.Context):
+async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
     """
-    TODO
-    https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.HelpCommand
-    """
-
-
-async def on_reaction(reaction: discord.Reaction, user: discord.Member):
-    """
-    Delete a quote, when the proper reaction [knife] is applied to a previous message by the bot
+    Delete a quote, when the proper reaction [knife] is applied to a message containing a quote
     """
     if reaction.emoji != "🔪":
         return
 
     message: discord.Message = reaction.message
 
-    await delete_quote(message)
+    try:
+        await delete_quote(message)
+    except QuoteMessageLog.DoesNotExist:
+        pass
+    else:
+        await message.channel.send("Message deleted!")
 
-    await message.channel.send("Message deleted!")
 
+bot.add_listener(on_reaction_add, "on_reaction_add")
 
-bot.add_listener(on_reaction, "on_reaction_add")
+bot.help_command
